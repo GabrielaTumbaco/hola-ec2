347099bbf4e5a88779b01e751f86e04aa068bc0d
GabrielaTumbaco <gltumbaco@uce.edu.ec>
2024-11-25 15:40:41 -0500

    Created a new program with Java Scipt

---
+++ holamundo.js
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hola Mundo</title>
+</head>
+<body>
+    <h1 id="mensaje"></h1>
+
+    <script>
+        // Hola Mundo en JavaScript
+        document.getElementById("mensaje").textContent = "Hola, mundo!";
+    </script>
+</body>
+</html>
